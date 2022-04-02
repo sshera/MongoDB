cf1337e063637603e2de0b8b30f9636fcc1de7b5
Sarita Shera <sarita.shera@gmail.com>
2022-04-01 21:17:15 -0700

    Initial commit

---
+++ cats.js
@@ -0,0 +1,68 @@
+cats = [
+    {
+        "name": "Cat1",
+        "age": "10 years",
+        "size": "large",
+        "breed": "Maine Coon",
+        "color": "brown",
+        "coat pattern": "tabby",
+    },
+    {
+        "name": "Cat2",
+        "age": "6 months",
+        "size": "small",
+        "breed": "Domestic Shorthair",
+        "color": [
+            "orange",
+            "white"
+        ],
+        "coat pattern": [
+            "tabby",
+            "bicolor"
+        ]
+    },
+    {
+        "name": "Cat3",
+        "age": "5 years",
+        "size": "medium",
+        "breed": "Siamese",
+        "color": "seal",
+        "coat pattern": "point",
+    },
+    {
+        "name": "Cat4",
+        "age": "18 years",
+        "size": "small",
+        "breed": "Domestic Shorthair",
+        "color": [
+            "black",
+            "red"
+        ],
+        "coat pattern": "tortoiseshell",
+    },
+    {
+        "name": "Cat5",
+        "age": "2 years",
+        "size": "large",
+        "breed": "Abyssinian",
+        "color": "ruddy",
+        "coat pattern": [
+            "tabby",
+            "ticked"
+        ],
+    },
+    {
+        "name": "Cat6",
+        "age": "8 years",
+        "size": "medium",
+        "breed": "Domestic Shorthair",
+        "color": [
+            "black",
+            "white"
+        ],
+        "coat pattern": [
+            "tuxedo",
+            "bicolor"
+        ]
+    }
+];
