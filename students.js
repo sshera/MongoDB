5813749172299d3e7e6adad2bce8962b220486d6
Sarita Shera <sarita.shera@gmail.com>
2022-04-01 21:11:10 -0700

    Completed MongoDB exercises.

---
+++ students.js
@@ -0,0 +1,140 @@
+student = {
+    "firstName": "Retha",
+    "lastName": "Killeen",
+    "email": "rkilleen@mysql.com",
+    "gender": "F",
+    "country": "Philippines",
+    "isStudentActive": false,
+    "favoriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentInBooks": 0.00
+}
+
+students = [
+    {
+        "firstName": "Retha",
+        "lastName": "Killeen",
+        "email": "rkilleen@mysql.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": false,
+        "favoriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentInBooks": 100.00
+    },
+    {
+        "firstName": "Coraline",
+        "lastName": "Langham",
+        "email": "clangham1@globo.com",
+        "gender": "F",
+        "country": "Philippines",
+        "isStudentActive": true,
+        "favoriteSubjects": [],
+        "totalSpentInBooks": 5.00
+    },
+    {
+        "firstName": "Ario",
+        "lastName": "Frye",
+        "email": "afrye2@blogs.com",
+        "gender": "M",
+        "country": "Argentina",
+        "isStudentActive": true,
+        "favoriteSubjects": [
+            "science",
+            "history"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Sandye",
+        "lastName": "Iddens",
+        "email": "siddens3@spiegel.de",
+        "gender": "F",
+        "country": "France",
+        "isStudentActive": true,
+        "favoriteSubjects": [
+            "it"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Lynn",
+        "lastName": "Antonsson",
+        "email": "lantonsson4@yelp.com",
+        "gender": "F",
+        "country": "Indonesia",
+        "isStudentActive": true,
+        "favoriteSubjects": [
+            "it",
+            "maths"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Fabe",
+        "lastName": "Chartman",
+        "email": "fchartman5@reuters.com",
+        "gender": "M",
+        "country": "Angola",
+        "isStudentActive": false,
+        "favoriteSubjects": [
+            "history"
+        ],
+        "totalSpentInBooks": 66.00
+    },
+    {
+        "firstName": "Nealon",
+        "lastName": "Tabord",
+        "email": "ntabord6@devhub.com",
+        "gender": "M",
+        "country": "Greece",
+        "isStudentActive": false,
+        "favoriteSubjects": [
+            "maths"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Jule",
+        "lastName": "Brough",
+        "email": "jbrough7@wix.com",
+        "gender": "M",
+        "country": "Russia",
+        "isStudentActive": true,
+        "favoriteSubjects": [
+            "english",
+            "science"
+        ],
+        "totalSpentInBooks": 100.00
+    },
+    {
+        "firstName": "Mufinella",
+        "lastName": "Sansun",
+        "email": "msansun8@google.com",
+        "gender": "F",
+        "country": "China",
+        "isStudentActive": false,
+        "favoriteSubjects": [
+            "science"
+        ],
+        "totalSpentInBooks": 0.00
+    },
+    {
+        "firstName": "Cally",
+        "lastName": "Walkden",
+        "email": "cwalkden9@craigslist.org",
+        "gender": "F",
+        "country": "Niger",
+        "isStudentActive": true,
+        "favoriteSubjects": [
+            "it"
+        ],
+        "totalSpentInBooks": 165.00
+    } 
+];
